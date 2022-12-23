16243f6c8dad136fc8d445e0e575132f1cbb9463
ibrahimHalilBesli <104659322+ibrahimHalilBesli@users.noreply.github.com>
2022-12-23 15:03:35 +0100

    creating

---
+++ src/day39_Recap/cydeo/Developer.java
@@ -0,0 +1,22 @@
+package day39_Recap.cydeo;
+
+public class Developer extends Employee{
+
+    public Developer(String name, int age, char gender, int employeeID, String jobTitle, double salary) {
+        super(name, age, gender, employeeID, jobTitle, salary);
+    }
+
+    public void fixingBugs(){
+
+        System.out.println(getJobTitle()+" "+getName()+" is fixing the bug");
+    }
+
+    @Override
+    public void work() {
+        System.out.println(getJobTitle()+" "+getName()+" is devoloping the application");
+    }
+
+
+
+
+}
