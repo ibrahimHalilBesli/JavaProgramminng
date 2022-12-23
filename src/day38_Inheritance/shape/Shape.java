1cef42301a2d9d9feec360290b53bdd4a9d98a8e
ibrahimHalilBesli <104659322+ibrahimHalilBesli@users.noreply.github.com>
2022-12-23 10:28:15 +0100

    updating

--- src/day38_Inheritance/shape/Shape.java
+++ src/day38_Inheritance/shape/Shape.java
@@ -2,31 +2,47 @@ package day38_Inheritance.shape;
 
 public class Shape {
 
-    public String name;
+    private String name;
 
+    public String getName() {
+        return name;
+    }
 
+    public void setName(String name) {
+        if(name == null){
+            System.err.println("Name can not be null");
+            System.exit(1); // 1: some thing went wrong
+        }
 
-    public Shape(String name) {
+        if(name.isEmpty() || name.isBlank()){
+            System.err.println("Invalid name");
+            System.exit(1);
+        }
+
         this.name = name;
     }
 
+
+    public Shape(String name) {
+        setName(name);
+    }
+
+
     public double area(){
-
-
-
         return 0;
     }
 
     public double perimeter(){
-
         return 0;
     }
 
+
+    @Override
     public String toString() {
         return "Shape{" +
                 "name='" + name + '\'' +
-                "Area='" + area() + '\'' +
-                "Perimeter='" + perimeter() + '\'' +
+                ", area='" + area() + '\'' +
+                ", perimeter='" + perimeter() + '\'' +
                 '}';
     }
 }
