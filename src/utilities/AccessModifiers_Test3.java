9d1ee371d3eb5fac609b3124da7a7a519bfc4c1f
ibrahimHalilBesli <104659322+ibrahimHalilBesli@users.noreply.github.com>
2022-12-24 15:33:27 +0100

    updated

---
+++ src/utilities/AccessModifiers_Test3.java
@@ -0,0 +1,11 @@
+package utilities;
+
+import day40_Finalkeyword.ProtectedAccessModifier;
+
+public class AccessModifiers_Test3 {
+
+    public static void main(String[] args) {
+
+        System.out.println(ProtectedAccessModifier.);
+    }
+}
